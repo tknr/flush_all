96cbc21b6329612f9aa74e4231db1bbe1fcd2597
tknr <tknr@server.tknr.com>
2020-09-23 13:50:39 +0900

    2020年  9月 23日 水曜日 13:50:39 JST

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p /usr/loca/bin/
+mkdir -p /usr/local/bin/
 ln -s `pwd`/renice_all /usr/local/bin/
 ln -s `pwd`/flush_cache	/usr/local/bin/
 ln -s `pwd`/jmap_hist_live_all /usr/local/bin/
